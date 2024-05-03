1725042db58c4626588d227754d542abb8b60d91
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-05-03 20:52:01 +0900

    :sparkles: 1025種類全て表示できるようにした

--- components/SearchBar.client.tsx
+++ components/SearchBar.client.tsx
@@ -7,37 +7,29 @@ import axios from "axios";
 export default function SearchBar() { 
   const [pokemonData, setPokemonData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const [offset, setOffset] = useState<number>(0);
+  const loadLimit = 20;
+  const maxPokemon = 1025;
 
   useEffect(() => {
-    const fetchPokemon = async () => {
-      const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
-      const promises = response.data.results.map(
-        async (pokemon: any) => {
-          const pokemonDetails = await axios.get(pokemon.url);
-          return pokemonDetails.data;
-        }
-      );
-      const results = await Promise.all(promises);
-      setPokemonData(results);
-      console.log(results);
-      setLoading(false);
-    }
     fetchPokemon();
   }, []);
 
-  // if (loading) {
-  //   return (
-  //     <div className="container mx-auto px-6 py-4 bg-blue-100">
-  //     <div className="flex items-center py-4">
-  //       <input type="text" placeholder="Pokemon Search" className="text-black rounded-l py-2 px-4 w-1/2 shadow-md rounded" />
-  //       <button className="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 mx-4 rounded-r shadow-md rounded">Search</button>
-  //     </div>
-  //     <div className="bg-white flex shadow-md rounded p-4 overflow-x-auto whitesace-nowrap">
-  //       <div className="text-black bg-gray-300 h-[100px] w-[100px] mx-4 flex items-center justify-center text-gray-700">Loading...</div>
-  //     </div>
-  //   </div>
-  //   )
-  // }
+  const fetchPokemon = async () => {
+    if (offset >= maxPokemon) return;
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${loadLimit}&offset=${offset}`);
+    const promises = response.data.results.map(
+      async (pokemon: any) => {
+        const pokemonDetails = await axios.get(pokemon.url);
+        return pokemonDetails.data;
+      }
+    );
+    const results = await Promise.all(promises);
+    setPokemonData(prev => [...prev, ...results]);
+    setOffset(prev => prev + loadLimit);
+    setLoading(false);
+  };
+
   return (
     <div className="container mx-auto px-6 py-4 bg-blue-100">
       <div className="flex items-center py-4">
@@ -46,18 +38,16 @@ export default function SearchBar() {
       </div>
       <div className="bg-white flex shadow-md rounded p-4 overflow-x-auto whitesace-nowrap">
         {loading 
-          ? <div className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center  shadow-md text-gray-400">Loading...</div>
+          ? <div className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center shadow-md text-gray-400">Loading...</div>
           : pokemonData.map(pokemon => (
             <img key={pokemon.id} className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center shadow-md" src={pokemon.sprites.other['official-artwork'].front_default} alt="Pokemon" />
           ))
         }
-        {/* {loading 
-          ? <div className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center  shadow-md text-gray-400">Loading...</div>
-          : pokemonData.map(pokemon => (
-            // <img key={pokemon.id}  src={pokemon.sprites.other['official-artwork'].front_default} alt="Pokemon" />
-            <PokemonImage key={pokemon.id} image={pokemon.sprites.other['official-artwork'].front_default} />
-          ))
-        } */}
+        {offset < maxPokemon && (
+          <button onClick={fetchPokemon} className="text-black h-[100px] w-[100px] flex shadow-md">
+              追加表示
+          </button>
+        )}
       </div>
     </div>
   );
