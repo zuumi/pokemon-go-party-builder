bcdf9e8d3f1d6c245f8123011c0d35f3f0289208
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-05-04 15:11:35 +0900

    :+1: ポケモンGOの画像データを表示するよう変更

--- components/PokemonImage.tsx
+++ components/PokemonImage.tsx
@@ -1,39 +1,22 @@
-"use client";
+// "use client";
 
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+// import React, { useState, useEffect } from "react";
 
 export default function PokemonImage({ pokemon }: {
   pokemon: any;
 }) {
-    const [japaneseName, setJapaneseName] = useState('');
-
-    useEffect(() => {
-      const fetchPokemonName = async () => {
-        try {
-          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon.name}`);
-          const japanese = response.data.names.find((name:any) => name.language.name === 'ja-Hrkt');
-          setJapaneseName(japanese ? japanese.name : '日本語名前がありません。');
-        } catch(err) {
-          setJapaneseName('');
-        }
-      };
-
-      fetchPokemonName();
-    },[pokemon.name]);
-
     const handleDragStart = (e: any) => {
-      e.dataTransfer.setData("text/plain", pokemon.id);
+      e.dataTransfer.setData("text/plain", pokemon.dexNr);
     };
-
+    console.log();
     return (
       <img
         draggable="true"
         onDragStart={handleDragStart}
-        key={pokemon.id}
-        className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center shadow-md"
-        src={pokemon.sprites.other['official-artwork'].front_default}
-        alt={japaneseName}
+        key={pokemon.dexNr}
+        className="text-black h-[120px] w-[250px] mx-4 flex items-center justify-center shadow-md"
+        src={pokemon.assets && pokemon.assets.image}
+        alt='japaneseName'
       />
     );
 }
--- components/SearchBar.client.tsx
+++ components/SearchBar.client.tsx
@@ -5,56 +5,38 @@ import axios from "axios";
 import PokemonImage from "./PokemonImage";
 
 export default function SearchBar() { 
-  const [pokemonData, setPokemonData] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
-  const [offset, setOffset] = useState<number>(0);
-  const [pokemonDict, setPokemonDict] = useState({ japanese: '', english: '' });
-  const [pokemonNames, setPokemonNames] = useState();
   const [searchTerm, setSearchTerm] = useState<string>("");
-  const loadLimit = 20;
-  const maxNormalPokemon = 1025;
-  const maxSpecialPokemon = 277;
-  const maxPokemon = maxNormalPokemon + maxSpecialPokemon;
+  const [pokemonGoData, setPokemonGoData] = useState<any[]>([]);
 
   useEffect(() => {
     try {
-      fetchPokemon();
-      fetchPokemonDict(2);
+      fetchPokemonGoData();
     } catch(err) {
       console.log(err);
     }
   }, []);
 
-  const fetchPokemon = async () => {
-    if (offset >= maxPokemon) return;
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${loadLimit}&offset=${offset}`);
-    const promises = response.data.results.map(
-      async (pokemon: any) => {
-        const pokemonDetails = await axios.get(pokemon.url);
-        return pokemonDetails.data;
-      }
-    );
-    const results = await Promise.all(promises);
-    setPokemonData(prev => [...prev, ...results]);
-    setOffset(prev => prev + loadLimit);
-    setLoading(false);
+  console.log(pokemonGoData);
+  const fetchPokemonGoData = async () => {
+    try{
+      const response = await axios.get(`https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json`);
+      const results = response.data.flatMap((pokemon: any) =>{
+        // if (pokemon.reginForms[`${pokemon.formId}_ALOHA`] && pokemon.reginForms[`${pokemon.formId}_GALARIAN`]) {
+        //   return [pokemon, ...pokemon.reginForms[`${pokemon.formId}_ALOHA`], ...pokemon.reginForms[`${pokemon.formId}_GALARIAN`]];
+        // } else if (pokemon.reginForms[`${pokemon.formId}_ALOHA`]) {
+        //   return [pokemon, ...pokemon.reginForms[`${pokemon.formId}_ALOHA`], ...pokemon.reginForms[`${pokemon.formId}_GALARIAN`]];
+        // } else if (pokemon.reginForms[`${pokemon.formId}_GALARIAN`]) {
+        //   return [pokemon, ...pokemon.reginForms[`${pokemon.formId}_ALOHA`], ...pokemon.reginForms[`${pokemon.formId}_GALARIAN`]];
+        // }
+        return pokemon;
+      });
+      setPokemonGoData(results);
+      setLoading(false);
+    } catch(err) {
+      console.log(`error:${err}`);
+    }
   };
-  
-    const fetchPokemonDict = async (number: number) => {
-      try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${number}`);
-        const names = response.data.names.map((name:any) => {return name});
-        const japaneseName = names.find((item: any) => {return item.language.name === 'ja-Hrkt' ? item.name : null});
-        const englishName = names.find((item: any) => {return item.language.name === 'en' ? item.name : null});
-        console.log(englishName.name.toLowerCase());
-        setPokemonDict({
-          japanese: japaneseName ? japaneseName.name : '日本語なし',
-          english: englishName ? englishName.name.toLowerCase() : '英語なし'
-        });
-      } catch(err) {
-        console.log(err);
-      }
-    };
 
   // const handleSearch = () => {
   //   const foundPokemon = pokemonNames.find(poke => poke.japaneseName === searchTerm);
@@ -85,15 +67,10 @@ export default function SearchBar() {
       <div className="bg-white flex shadow-md rounded p-4 overflow-x-auto whitesace-nowrap">
         {loading 
           ? <div className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center shadow-md text-gray-400">Loading...</div>
-          : pokemonData.map(pokemon => (
-            <PokemonImage key={pokemon.id} pokemon={pokemon}/>
+          : pokemonGoData.map(pokemon => (
+            <PokemonImage key={pokemon.dexNr} pokemon={pokemon}/>
           ))
         }
-        {offset < maxPokemon && (
-          <button onClick={fetchPokemon} className="text-black h-[100px] w-[100px] mx-4 flex items-center justify-center shadow-md">
-              追加表示
-          </button>
-        )}
       </div>
     </div>
   );
