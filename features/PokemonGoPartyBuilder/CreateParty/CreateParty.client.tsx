34977cb0ae99fd8c189fbfdc0a9843d97cdd9255
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-05-06 21:13:16 +0900

    coming soon..

--- features/PokemonGoPartyBuilder/CreateParty/CreateParty.client.tsx
+++ features/PokemonGoPartyBuilder/CreateParty/CreateParty.client.tsx
@@ -57,9 +57,9 @@ export default function CreateParty() {
     <div className="container mx-auto px-6 py-4 bg-blue-100 shadow-md rounded h-full">
       <div className="container mx-auto py-4">
         <div className="flex items-center bg-blue-100">
-          {/* Comming Soooooon!!! */}
-          <input type="text" placeholder="Party Name" className="text-black rounded-l py-2 px-4 w-1/2 shadow-md rounded" />
-          <button className="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 mx-4 rounded-r shadow-md rounded">Create</button>
+          {/* Coming Soooooon!!! */}
+          {/* <input type="text" placeholder="Party Name" className="text-black rounded-l py-2 px-4 w-1/2 shadow-md rounded" />
+          <button className="ml-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 mx-4 rounded-r shadow-md rounded">Create</button> */}
         </div>
       </div>
       <div className="bg-white shadown-md rounded p-4 flex justify-between space-x-4 overflow-x-auto whitespace-nowrap">
