d5aee1f13af6309cba63487e7eaf9ca37d3c433d
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-05-25 13:22:35 +0900

    :recycle: ファイルの大文字小文字を認識されてなかったのでなおした

---
+++ features/PokemonGoPartyBuilder/CreateParty/ChatService.tsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const API_URL = 'https://api.openai.com/v1/';
+const MODEL = 'gpt-4o';
+
+export const gptChat = async (message: string) => {
+  const API_KEY = process.env["NEXT_PUBLIC_OPENAI_API_KEY"];
+  try {
+    const response = await axios.post( `${ API_URL }chat/completions`, {
+      // モデル ID の指定
+      model: MODEL,
+      // 質問内容
+      messages: [
+        {
+          'role': 'user',
+          'content': message,
+        }
+      ],
+    }, {
+      // 送信する HTTP ヘッダー(認証情報)
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${ API_KEY }`
+      }
+    });
+    // 回答の取得
+    return response.data.choices[0].message.content;
+
+  } catch ( error ) {
+    console.error( error );
+    return null;
+  }
+}
