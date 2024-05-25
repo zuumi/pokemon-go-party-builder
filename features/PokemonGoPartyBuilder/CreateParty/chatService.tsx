b5135cd7b313d5a3aefbb07531dcf5404f31b5c6
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-05-25 12:56:17 +0900

    :+1: gpt-4oの性能を試してみる

--- features/PokemonGoPartyBuilder/CreateParty/chatService.tsx
+++ features/PokemonGoPartyBuilder/CreateParty/chatService.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://api.openai.com/v1/';
-const MODEL = 'gpt-4-turbo-2024-04-09';
+const MODEL = 'gpt-4o';
 
 export const gptChat = async (message: string) => {
   const API_KEY = process.env["NEXT_PUBLIC_OPENAI_API_KEY"];
