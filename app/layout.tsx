ad8c1d8b519ab4287d1cbfa9a4d9669582aa10ef
zuumi <k0w8z0m6y1sh3k@gmail.com>
2024-06-28 23:01:06 +0900

    :+1: Vercelのアナリティクスを導入

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import NextAuthProvider from "./providers";
 
 import Header from "../components/Header";
 import { nextAuthOptions } from "../lib/next-auth-options";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
